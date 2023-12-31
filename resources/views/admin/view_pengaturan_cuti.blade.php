d1fb0c4a704468fa3523cac5308728a9b59586d6
ajil23 <azrilpraya@gmail.com>
2023-12-31 18:17:15 +0800

    update view for admin

--- resources/views/admin/view_pengaturan_cuti.blade.php
+++ resources/views/admin/view_pengaturan_cuti.blade.php
@@ -7,44 +7,22 @@
         <div class="col">
             <h1 class="h3 mb-2 text-gray-800">Pengaturan Cuti</h1>
         </div>
-        <div class="text-end mb-2">
-            <a href="#"><button type="button" class="btn btn-primary">Pengajuan Cuti</button></a>
-        </div>
     </div>
 
      <!-- DataTales Example -->
      <div class="card shadow mb-4">
          <div class="card-header py-3">
-             <h6 class="m-0 font-weight-bold text-primary">Tabel Pelaksanaan</h6>
+             <h6 class="m-0 font-weight-bold text-primary">Batas Waktu Pengajuan Cuti</h6>
          </div>
          <div class="card-body">
-             <div class="table-responsive">
-                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                     <thead>
-                         <tr>
-                             <th>No</th>
-                             <th>Kegiatan</th>
-                             <th>Progres</th>
-                             <th>Realisasi</th>
-                             <th>Dokumen Laporan</th>
-                             <th>Aksi</th>
-                         </tr>
-                     </thead>
-                     <tbody>
-                        <tr>
-                            <td>{{$loop->iteration}}</td>
-                            <td>fd</td>
-                            <td>sdf</td>
-                            <td>Rp. 43</td>
-                            <td>file</td>
-                            <td colspan="2">
-                               <a href="" class="btn btn-warning">Edit</a>
-                               <a href="" class="btn btn-danger">Hapus</a>
-                            </td>
-                        </tr>
-                     </tbody>
-                 </table>
-             </div>
+            <form action="#">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Batas Waktu Pengajuan Cuti</label>
+                    <input type="number" class="form-control" id="exampleFormControlInput1">
+                    <small>*Pengajuan cuti yang belum selesai  akan otomatis dibatalkan jika melewati batas waktu</small>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button> 
+            </form>                     
          </div>
      </div>
 </div>
